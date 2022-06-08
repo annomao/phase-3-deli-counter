c6044b95d71614ac5e0bdb2828857931707f1621
annomao <omaoannkerubo@gmail.com>
2022-06-08 09:23:48 +0300

    commit tree 918df881bd740ca89e632a67e5cee6e94dc5a758

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+katz_deli = []
+
+def line (array)
+  if array.length == 0
+    puts "The line is currently empty."
+  elsif array.length > 0
+   new_array = array.map.with_index(1) do |value,index|
+       "#{index}. #{value}"
+   end
+  puts "The line is currently: " + "#{new_array.join(" ")}"
+  end
+end
+
+def take_a_number (array , name)
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+  array.push(name)
+end
+
+def now_serving (array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  elsif array.length > 0
+  puts "Currently serving #{array.first}."
+  array.shift()
+  end
+end
